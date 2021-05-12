90d2a89e41507f1a7775813e358045380be9bbca
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-05-12 13:13:45 +0100

    Update

--- Network/SharedServices/resource_network.tf
+++ Network/SharedServices/resource_network.tf
@@ -11,7 +11,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "Subnet-${index(var.subnet_prefix, each.value) + 1}"
   resource_group_name  = azurerm_resource_group.resource_group.name
   virtual_network_name = azurerm_virtual_network.virtual_network.name
-  address_prefixes     = each.value
+  address_prefixes     = [each.value]
 }
 
 /* resource "azurerm_route_table" "route_table" {
