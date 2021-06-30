1537192b92e5aa07d80bda9846ca150efb39434f
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-06-30 17:12:14 +0100

    Update

--- Network/SharedServices/resource_network.tf
+++ Network/SharedServices/resource_network.tf
@@ -27,7 +27,7 @@ resource "azurerm_route_table" "route_table" {
 }
 
 resource "azurerm_subnet_route_table_association" "association" {
-  for_each       = toset(azurerm_subnet.subnet.name)
-  subnet_id      = azurerm_subnet.subnet[each.key].id
+  for_each       = azurerm_subnet.subnet
+  subnet_id      = each.value.id
   route_table_id = azurerm_route_table.route_table.id
 }
