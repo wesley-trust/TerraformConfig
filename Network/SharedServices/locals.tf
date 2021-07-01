caf1a8d143eb3d8c208e0c8d88e0138996f27d28
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-07-01 16:06:48 +0100

    Test

--- Network/SharedServices/locals.tf
+++ Network/SharedServices/locals.tf
@@ -1,6 +1,4 @@
 # Calculate local variables
 locals {
-  description          = "Calculated subnet prefixes for the provisioned resources"
-  type                 = list(string)
   resource_cidrsubnets = cidrsubnets(var.resource_address_space, 1, 1)
 }
