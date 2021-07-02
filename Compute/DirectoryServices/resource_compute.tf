ab6a08a949d7704aba84b2b91e97fed579f94af9
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-07-02 12:38:45 +0100

    Update

--- Compute/DirectoryServices/resource_compute.tf
+++ Compute/DirectoryServices/resource_compute.tf
@@ -25,7 +25,7 @@ resource "azurerm_windows_virtual_machine" "virtual_machine" {
   network_interface_ids = [
     element(azurerm_network_interface.network_interface.*.id, count.index),
   ]
-  zone = count.index % lookup(var.resource_location_az, azurerm_resource_group.resource_group.location, null)
+  zone = count.index % lookup(var.resource_location_az, var.resource_location, null)
 
   os_disk {
     caching              = "ReadWrite"
