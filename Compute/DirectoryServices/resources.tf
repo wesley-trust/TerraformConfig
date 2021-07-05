f2b0e2d978a904b070d688eafc79053569d2c4fb
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-07-05 16:42:56 +0100

    Test

--- Compute/DirectoryServices/resources.tf
+++ Compute/DirectoryServices/resources.tf
@@ -1,7 +1,15 @@
-# Create virtual machine
+# Create production virtual machine
 module "virtual_machine_prod" {
   source              = "../Modules/Compute_az"
   service_name        = var.service_name
-  service_environment = var.service_environment
+  service_environment = "Prod"
+  service_deployment  = var.service_deployment
+}
+
+# Create virtual machine
+module "virtual_machine_prod_dr" {
+  source              = "../Modules/Compute_az"
+  service_name        = var.service_name
+  service_environment = "Prod-DR"
   service_deployment  = var.service_deployment
 }
