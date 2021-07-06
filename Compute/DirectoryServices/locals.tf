96684efe81ef9c5df93732fff576d79703b417eb
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-07-06 14:48:00 +0100

    Update

--- Compute/DirectoryServices/locals.tf
+++ Compute/DirectoryServices/locals.tf
@@ -1,11 +1,16 @@
 # Calculate local variables
 locals {
-  # Concatenate, lookup the name based upon the lookup of the environment, location and service
-  resource_prod_locations    = lookup(var.resource_location, "Prod", null)
-  resource_name              = lookup(var.resource_name, var.service_name, null)
-  resource_instance_count    = lookup(var.resource_instance_count, "Prod", null)
-  resource_instance_size     = lookup(var.resource_instance_size, var.service_name, null)
-  resource_network_name      = lookup(var.resource_network_name, var.service_name, null)
-  resource_subnet_name       = lookup(var.resource_subnet_name, var.service_name, null)
-  resource_prod-dr_locations = lookup(var.resource_location, "Prod-DR", null)
+  # Resource
+  resource_name          = lookup(var.resource_name, var.service_name, null)
+  resource_instance_size = lookup(var.resource_instance_size, var.service_name, null)
+  resource_network_name  = lookup(var.resource_network_name, var.service_name, null)
+  resource_subnet_name   = lookup(var.resource_subnet_name, var.service_name, null)
+
+  # Production Specific
+  resource_prod_locations      = lookup(var.resource_location, "Prod", null)
+  resource_prod_instance_count = lookup(var.resource_instance_count, "Prod", null)
+
+  # Production DR Specific
+  resource_prod_dr_locations      = lookup(var.resource_location, "Prod-DR", null)
+  resource_prod_dr_instance_count = lookup(var.resource_instance_count, "Prod-DR", null)
 }
--- Compute/DirectoryServices/resources.tf
+++ Compute/DirectoryServices/resources.tf
@@ -1,27 +1,27 @@
 # Create production virtual machines
 module "virtual_machine_prod" {
-  service_environment     = "Prod"
   for_each                = toset(local.resource_prod_locations)
-  source                  = lookup(var.source_compute, lookup(var.availability_location_support, each.value, null), null)
+  service_environment     = "Prod"
+  source                  = "../Modules/Compute_az"
   service_name            = var.service_name
   service_location        = each.value
   service_deployment      = format("%02d", index(local.resource_prod_locations, each.value) + 1)
   resource_name           = local.resource_name
-  resource_instance_count = local.resource_instance_count
+  resource_instance_count = local.resource_prod_instance_count
   resource_instance_size  = local.resource_instance_size
   resource_network_name   = local.resource_network_name
   resource_subnet_name    = local.resource_subnet_name
 }
 
 module "virtual_machine_prod_dr" {
+  for_each                = toset(local.resource_prod_dr_locations)
   service_environment     = "Prod-DR"
-  for_each                = toset(local.resource_prod_locations)
-  source                  = lookup(var.source_compute, lookup(var.availability_location_support, each.value, null), null)
+  source                  = "../Modules/Compute_as"
   service_name            = var.service_name
   service_location        = each.value
   service_deployment      = format("%02d", index(local.resource_prod_dr_locations, each.value) + 1)
   resource_name           = local.resource_name
-  resource_instance_count = local.resource_instance_count
+  resource_instance_count = local.resource_prod_dr_instance_count
   resource_instance_size  = local.resource_instance_size
   resource_network_name   = local.resource_network_name
   resource_subnet_name    = local.resource_subnet_name
