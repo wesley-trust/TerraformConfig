c07c1ca738a5f775b69e885e2914c063c3251702
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-07-06 16:55:01 +0100

    Update

---
+++ Compute/FileServices/locals.tf
@@ -0,0 +1,17 @@
+# Calculate local variables
+locals {
+  # Resource
+  resource_name               = lookup(var.resource_name, var.service_name, null)
+  resource_instance_size      = lookup(var.resource_instance_size, var.service_name, null)
+  resource_network_name       = lookup(var.resource_network_name, var.service_name, null)
+  resource_network_deployment = lookup(var.resource_network_deployment, var.service_name, null)
+  resource_subnet_name        = lookup(var.resource_subnet_name, var.service_name, null)
+
+  # Production Specific
+  resource_prod_locations      = lookup(var.service_location, "Prod", null)
+  resource_prod_instance_count = lookup(var.resource_instance_count, "Prod", null)
+
+  # Production DR Specific
+  resource_prod_dr_locations      = lookup(var.service_location, "Prod-DR", null)
+  resource_prod_dr_instance_count = lookup(var.resource_instance_count, "Prod-DR", null)
+}
---
+++ Compute/FileServices/variables.tf
@@ -0,0 +1,74 @@
+# Define variables
+variable "service_name" {
+  description = "Desired name for the provisioned resources"
+  type        = string
+  default     = "FileServices"
+}
+
+variable "service_location" {
+  description = "The production resource locations to deploy"
+  type        = map(any)
+  default = {
+    Prod = [
+      "UK South",
+    ]
+
+    Prod-DR = [
+      "North Europe",
+    ]
+  }
+}
+
+variable "resource_name" {
+  description = "Desired name for the provisioned resources"
+  type        = map(string)
+  default = {
+    "DirectoryServices" = "ACD"
+    "FileServices"      = "FS"
+  }
+}
+
+variable "resource_instance_count" {
+  description = "Desired number of the provisioned resources for each service environment"
+  type        = map(string)
+  default = {
+    "Prod"    = 2
+    "Prod-DR" = 1
+  }
+}
+
+variable "resource_instance_size" {
+  description = "Desired size for the provisioned resources for each service"
+  type        = map(string)
+  default = {
+    "DirectoryServices" = "Standard_B1s"
+    "FileServices"      = "Standard_B1s"
+  }
+}
+
+variable "resource_network_name" {
+  description = "The network name for the provisioned resources"
+  type        = map(string)
+  default = {
+    "DirectoryServices" = "SharedServices"
+    "FileServices"      = "SharedServices"
+  }
+}
+
+variable "resource_network_deployment" {
+  description = "The network deployment for the provisioned resources"
+  type        = map(string)
+  default = {
+    "DirectoryServices" = "01"
+    "FileServices"      = "01"
+  }
+}
+
+variable "resource_subnet_name" {
+  description = "The subnet name for the provisioned resources"
+  type        = map(string)
+  default = {
+    "DirectoryServices" = "InfrastructureSubnet-1"
+    "FileServices"      = "InfrastructureSubnet-1"
+  }
+}
