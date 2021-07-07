3a2e295005b5ea4d7355522a7293caf4fc22bed9
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-07-07 17:19:22 +0100

    Update

--- Network/SharedServices/locals.tf
+++ Network/SharedServices/locals.tf
@@ -3,9 +3,7 @@ locals {
 
   # Production Specific
   resource_prod_locations    = lookup(var.service_location, "Prod", null)
-  resource_prod_subnet_count = lookup(var.resource_subnet_count, "Prod", null)
 
   # Production DR Specific
   resource_prod_dr_locations    = lookup(var.service_location, "Prod-DR", null)
-  resource_prod_dr_subnet_count = lookup(var.resource_subnet_count, "Prod-DR", null)
 }
