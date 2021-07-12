e107ae547854e60183cf2ac5cb636645ad1f066c
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-07-12 10:31:05 +0100

    Update

--- FileServices/locals.tf
+++ FileServices/locals.tf
@@ -3,8 +3,6 @@ locals {
   # Resource
   resource_name          = lookup(var.resource_name, var.service_name, null)
   resource_instance_size = lookup(var.resource_instance_size, var.service_name, null)
-  resource_address_space = lookup(var.resource_address_space, each.value, null)
-  resource_subnet_name   = lookup(var.resource_subnet_name, each.value, null)
 
   # Production Specific
   resource_prod_locations      = lookup(var.service_location, "Prod", null)
