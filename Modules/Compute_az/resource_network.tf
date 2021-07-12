ca2f7dc990fe08f0bbb1d8e86afcd2f60190edb5
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-07-12 11:55:44 +0100

    Update

--- Modules/Compute_az/resource_network.tf
+++ Modules/Compute_az/resource_network.tf
@@ -26,7 +26,6 @@ resource "azurerm_route_table" "route_table" {
 }
 
 resource "azurerm_subnet_route_table_association" "association" {
-  for_each       = azurerm_subnet.subnet
-  subnet_id      = each.value.id
+  subnet_id      = azurerm_subnet.subnet.id
   route_table_id = azurerm_route_table.route_table.id
 }
