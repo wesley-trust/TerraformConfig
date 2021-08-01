58bbfa54c81cefbb4dc6fd3611b6e0cfc3ca4cfe
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-01 12:34:23 +0100

    Update

--- Modules/Compute/Linux_virtual_machine/resource_compute.tf
+++ Modules/Compute/Linux_virtual_machine/resource_compute.tf
@@ -20,12 +20,13 @@ resource "azurerm_linux_virtual_machine" "virtual_machine" {
   count = var.resource_instance_count
 
   # Format with leading zero
-  name                = "${local.resource_name}${format("%02d", count.index + 1)}-vm"
-  location            = module.resource_group.location
-  resource_group_name = module.resource_group.name
-  size                = var.resource_instance_size
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
+  name                            = "${local.resource_name}${format("%02d", count.index + 1)}-vm"
+  location                        = module.resource_group.location
+  resource_group_name             = module.resource_group.name
+  size                            = var.resource_instance_size
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
 
   # Get all of the interface ids, and select the correct one for this iteration
   network_interface_ids = element(module.network_interfaces.*.network_interface_ids, count.index)
