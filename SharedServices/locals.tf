fce2e78b45c408731097ebc24955dd25e297b8bb
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-01 14:32:12 +0100

    Update

--- SharedServices/locals.tf
+++
@@ -1,9 +0,0 @@
-# Calculate local variables
-locals {
-
-  # Prod Specific
-  resource_prod_locations    = lookup(var.service_location, "Prod", null)
-
-  # Prod DR Specific
-  resource_prod_dr_locations    = lookup(var.service_location, "Prod-DR", null)
-}
