b71ceaa8b3f670e1e49ec17566d2c42d21b51816
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-14 15:03:02 +0100

    Remove resources

--- ManagementServices/resources.tf
+++ ManagementServices/resources.tf
@@ -1,4 +1,4 @@
-module "management_services_prod" {
+/* module "management_services_prod" {
   for_each                       = toset(local.resource_prod_locations)
   source                         = "../Modules/Deployments/Windows_virtual_machine"
   service_environment            = "Prod"
@@ -38,3 +38,4 @@ module "management_services_recovery_services_prod" {
   resource_recovery_services_instance_count   = local.resource_prod_recovery_services_instance_count
   resource_recovery_services_virtual_machines = module.management_services_prod[each.value]
 }
+ */
