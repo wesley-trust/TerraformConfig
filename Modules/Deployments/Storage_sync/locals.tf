44af90f9a69836700002e355ddcea9b3175627e2
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-15 19:06:44 +0100

    Update storage account name

--- Modules/Deployments/Storage_sync/locals.tf
+++ Modules/Deployments/Storage_sync/locals.tf
@@ -6,7 +6,7 @@ locals {
 
   # Resource functions and concatenations
   resource_name                             = "${local.service_environment_prefix}-${local.service_location_prefix}-${var.resource_name}"
-  resource_storage_account_name             = lower("${var.service_name}${var.service_environment}sa${random_id.id.hex}")
+  resource_storage_account_name             = lower("${var.resource_name}${var.service_environment}sa${random_id.id.hex}")
   resource_storage_share_name               = lower("${local.resource_name}-share")
   resource_storage_sync_name                = "${local.resource_name}-ss"
   resource_storage_sync_group_name          = "${local.resource_storage_sync_name}-gp"
