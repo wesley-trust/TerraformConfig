a26ea21a18bbc799099a090e7e4340bfb69ff43f
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-16 10:12:51 +0100

    Test deployment

---
+++ Modules/Deployments/Storage_sync/resources_role_assignment.tf
@@ -0,0 +1,15 @@
+resource "azurerm_role_assignment" "reader" {
+  #name               = "00000000-0000-0000-0000-000000000000"
+  scope              = azurerm_storage_account.account.id
+  #role_definition_id = azurerm_role_definition.example.role_definition_resource_id
+  role_definition_name = "Reader"
+  principal_id       = "a7ddc9cc-daee-4e0a-be3f-027903915879"
+}
+
+resource "azurerm_role_assignment" "data_access" {
+  #name               = "00000000-0000-0000-0000-000000000000"
+  scope              = azurerm_storage_account.account.id
+  #role_definition_id = azurerm_role_definition.example.role_definition_resource_id
+  role_definition_name = "Data Access"
+  principal_id       = "a7ddc9cc-daee-4e0a-be3f-027903915879"
+}
---
+++ Modules/Deployments/Storage_sync/resources_storage_sync.tf
@@ -0,0 +1,18 @@
+resource "azurerm_storage_sync" "sync" {
+  depends_on          = [azurerm_resource_provider_registration.storage_sync]
+  name                = local.resource_storage_sync_name
+  resource_group_name = module.resource_group.name
+  location            = module.resource_group.location
+}
+
+resource "azurerm_storage_sync_group" "sync_group" {
+  name            = local.resource_storage_sync_group_name
+  storage_sync_id = azurerm_storage_sync.sync.id
+}
+
+resource "azurerm_storage_sync_cloud_endpoint" "sync_cloud_endpoint" {
+  name                  = local.resource_storage_sync_cloud_endpoint_name
+  storage_sync_group_id = azurerm_storage_sync_group.sync_group.id
+  file_share_name       = azurerm_storage_share.share.name
+  storage_account_id    = azurerm_storage_account.account.id
+}
