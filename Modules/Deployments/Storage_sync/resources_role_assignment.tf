a9956088fe06ed0afb63a0ac2ffd24f6843a9220
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-16 11:44:50 +0100

    Change permissions

--- Modules/Deployments/Storage_sync/resources_role_assignment.tf
+++ Modules/Deployments/Storage_sync/resources_role_assignment.tf
@@ -1,15 +1,7 @@
-resource "azurerm_role_assignment" "reader" {
-  #name               = "00000000-0000-0000-0000-000000000000"
-  scope              = azurerm_storage_account.account.id
-  #role_definition_id = azurerm_role_definition.example.role_definition_resource_id
-  role_definition_name = "Reader"
-  principal_id       = "a7ddc9cc-daee-4e0a-be3f-027903915879"
-}
-
 resource "azurerm_role_assignment" "data_access" {
   #name               = "00000000-0000-0000-0000-000000000000"
   scope              = azurerm_storage_account.account.id
   #role_definition_id = azurerm_role_definition.example.role_definition_resource_id
-  role_definition_name = "Data Access"
+  role_definition_name = "Reader and Data Access"
   principal_id       = "a7ddc9cc-daee-4e0a-be3f-027903915879"
 }
