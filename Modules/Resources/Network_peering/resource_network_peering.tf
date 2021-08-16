1284bbdf33d6bb21cc8fd6a480efbad9cc8bd589
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-16 20:19:17 +0100

    Update naming

--- Modules/Resources/Network_peering/resource_network_peering.tf
+++ Modules/Resources/Network_peering/resource_network_peering.tf
@@ -1,5 +1,5 @@
-resource "azurerm_virtual_network_peering" "from_service_to_peer" {
-  name                = "From-${var.service_name}-To-${resource_network_name_peer}"
+resource "azurerm_virtual_network_peering" "from_hub_to_peer" {
+  name                = "From-${var.resource_network_name}-To-${var.resource_network_name_peer}"
   resource_group_name = var.resource_group_name
   # Source
   virtual_network_name = var.resource_network_name
@@ -7,8 +7,8 @@ resource "azurerm_virtual_network_peering" "from_service_to_peer" {
   remote_virtual_network_id = data.azurerm_virtual_network.peer.id
 }
 
-resource "azurerm_virtual_network_peering" "to_service_from_peer" {
-  name                = "To-${var.service_name}-From-${resource_network_name_peer}"
+resource "azurerm_virtual_network_peering" "from_peer_to_hub" {
+  name                = "From-${var.resource_network_name_peer}-To-${var.resource_network_name}"
   resource_group_name = var.resource_network_name_peer
   # Source
   virtual_network_name = data.azurerm_virtual_network.peer.name
