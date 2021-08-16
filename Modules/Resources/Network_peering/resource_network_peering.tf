f3887252940b4c8b931d775ecb09c440a6122b57
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-16 20:32:04 +0100

    Update argument

--- Modules/Resources/Network_peering/resource_network_peering.tf
+++ Modules/Resources/Network_peering/resource_network_peering.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network_peering" "from_hub_to_peer" {
 
 resource "azurerm_virtual_network_peering" "from_peer_to_hub" {
   name                = "From-${var.resource_network_name_peer}-To-${var.resource_network_name}"
-  resource_group_name = var.resource_network_name_peer
+  resource_group_name = var.resource_group_name_peer
   # Source
   virtual_network_name = data.azurerm_virtual_network.peer.name
   # Destination
