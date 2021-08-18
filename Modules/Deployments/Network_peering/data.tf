3b610ac94a74e888c9b7617f51b52dd31d8f47ac
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-18 19:16:08 +0100

    Update

--- Modules/Deployments/Network_peering/data.tf
+++ Modules/Deployments/Network_peering/data.tf
@@ -3,7 +3,7 @@ data "azurerm_resources" "virtual_network_spokes" {
   type = "Microsoft.Network/virtualNetworks"
 
   required_tags = {
-    environment = var.service_environment
-    role        = var.resource_network_peer_role
+    environment = "Prod"
+    role        = "spoke"
   }
 }
