2e1c65d50f4b31ebca6fecba7d5731e0b59b33a0
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-21 17:54:24 +0100

    Remove resources

--- DirectoryServices/resources.tf
+++ DirectoryServices/resources.tf
@@ -1,4 +1,4 @@
-module "directory_services_virtual_machines_prod" {
+/* module "directory_services_virtual_machines_prod" {
   for_each                = toset(local.resource_prod_locations)
   source                  = "../Modules/Deployments/Windows_virtual_machine"
   service_environment     = "Prod"
@@ -61,4 +61,4 @@ module "directory_services_network_peering_prod_dr" {
   service_location           = each.value
   resource_network_peer      = module.directory_services_virtual_machines_prod_dr[each.value]
   resource_network_peer_role = var.resource_network_peer_role
-}
+} */
