a44284bfcd565362511e4a1462c17f49c3114935
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-21 17:05:23 +0100

    Deploy

--- DirectoryServices/resources.tf
+++ DirectoryServices/resources.tf
@@ -1,4 +1,4 @@
-/* module "directory_services_virtual_machines_prod" {
+module "directory_services_virtual_machines_prod" {
   for_each                = toset(local.resource_prod_locations)
   source                  = "../Modules/Deployments/Windows_virtual_machine"
   service_environment     = "Prod"
@@ -22,7 +22,7 @@ module "directory_services_network_peering_prod" {
   service_location           = each.value
   resource_network_peer      = module.directory_services_virtual_machines_prod[each.value]
   resource_network_peer_role = var.resource_network_peer_role
-} */
+}
 
 /* module "directory_services_recovery_services_prod" {
   depends_on                                  = [module.directory_services_virtual_machines_prod]
