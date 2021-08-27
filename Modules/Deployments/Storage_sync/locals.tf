87478b1d3c93cca62e11365b5de53e0511135685
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-27 19:13:54 +0100

    Update

--- Modules/Deployments/Storage_sync/locals.tf
+++ Modules/Deployments/Storage_sync/locals.tf
@@ -5,7 +5,7 @@ locals {
   service_location_prefix    = replace(var.service_location, "/[a-z[:space:]]/", "")
 
   # Resource functions and concatenations
-  resource_name                             = "${local.service_environment_prefix}-${local.service_location_prefix}-${var.resource_name}"
+  resource_name                             = "${local.service_environment_prefix}-${local.service_location_prefix}-${var.service_deployment}-${var.resource_name}"
   resource_storage_account_name             = lower("${var.resource_name}ss${var.service_environment}sa${random_id.id.hex}")
   resource_storage_share_name               = lower("${local.resource_name}-share")
   resource_storage_sync_name                = "${local.resource_name}-ss"
